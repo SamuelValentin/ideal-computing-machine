92a2b6c202ba0fad1d49382d7a4de23e0f301db9
SamuelValentin <62106538+SamuelValentin@users.noreply.github.com>
2022-07-18 21:12:04 -0300

    Add files via upload

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale-1">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Laracasts Videos Games</title>
+</head>
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-cente justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="/laracasts-logo.svg" alt="laracasts" class="w-32 flex-none">
+                </a>
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" class="bg-gray-800 text-sm rounded-full px-3 py-1" placeholder="Search..">
+                </div>
+                <div class="ml-6">
+                    <a href="#"><img src="/avatar.jpeg" alt="avatar" class="rounded-full w-8"></a>
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ storage/framework/views/83b9b5e92d968c2a927b6669745eb7f24162fe2a.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('content'); ?>
+    index here
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/resources/views/index.blade.php ENDPATH**/ ?>
